3fd6a8097c1c7ff852e5a1d22ffbb7bf1248e72a
beingbook@gmail.com <beingbook@gmail.com>
2016-03-12 01:02:51 +0900

    Fix test env for windows

--- tests/helper.js
+++ tests/helper.js
@@ -1,6 +1,10 @@
 import { expect } from 'chai';
+import { addPath } from 'app-module-path';
+import path from 'path';
 
 global.expect = expect;
 
 global.__DEV__ = false;
 global.__PORT__ = 8111;
+
+addPath(path.resolve(__dirname, '../src'));
